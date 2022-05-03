5edb693a2eeb8de6cfc1059b5f138b04cf501051
Chrisu <89726956+ultimatechrisu@users.noreply.github.com>
2022-05-02 23:44:38 -0400

    Add files via upload

--- guessing game/app.js
+++ guessing game/app.js
@@ -1,54 +1,46 @@
-let guessResults = document.querySelector('#results')
-const guessCountArea = document.querySelector('#guesses')
-const fTryArea = document.querySelector('#firstTry')
-const correctArea = document.querySelector('#correct')
-const totGuessesArea = document.querySelector('#total')
-const luckArea = document.querySelector('#luck')
-let guessCount = 0
-let fTry = 0
-let numRight = 0
 
-
-let totGuesses = 0
-
-document.querySelector('#rButton').addEventListener('click', ()=>{
-    location.reload()
-})
-
-document.querySelectorAll('.box').forEach(item =>{
+//Per click of boxes
+allBoxes.forEach(item =>{
     item.addEventListener('click', e =>{
-        let randNum = Math.floor(Math.random() * 6) + 1
+           
         const userChoice = parseInt(e.target.textContent) 
         
         totGuesses++
         totGuessesArea.innerHTML = `Total guesses: ${totGuesses}`
-        
 
         if(userChoice === randNum && guessCount === 0){
             guessResults.innerHTML = `You guessed: ${userChoice}.<br>
-             The number was ${randNum}.<br>
-              It took you ${guessCount + 1} try.<br>
-              You Win!`, guessResults.style.color='green', guessCount = 0, fTry++, 
-              fTryArea.innerHTML = `Number of first tries: ${fTry}`, numRight++
+             The number was ${randNum}. It took you ${guessCount + 1} try.<br>
+              You Win!<br> <span id="resPrompt">Restarting. . .</span>`, guessResults.style.color='green', guessCount = 0, fTry++, 
+              fTryArea.innerHTML = `Number of first tries: ${fTry}`, numRight++, e.target.style.color ='purple' , e.target.style.outlineColor ='purple', 
+              randNum = Math.floor(Math.random() * 6) + 1, gameWinTimeout(), boxesDisabled(),
+              document.querySelector('#resPrompt').style.color ="white"
         } 
         
         else if(userChoice === randNum){
             guessResults.innerHTML = `You guessed: ${userChoice}.<br>
-             The number was ${randNum}.<br>
-              It took you ${guessCount + 1} tries.<br>
-              You Win!`, guessResults.style.color='green', guessCount = 0, numRight++
+             The number was ${randNum}. It took you ${guessCount + 1} tries.<br>
+              You Win!<br> <span id="resPrompt">Restarting. . .</span>`, guessResults.style.color='green', guessCount = 0, numRight++, e.target.style.color ='green' , e.target.style.outlineColor ='green',
+              randNum = Math.floor(Math.random() * 6) + 1, gameWinTimeout(), boxesDisabled(),
+              document.querySelector('#resPrompt').style.color ="white"
         } 
-        
+        else if(userChoice !== randNum && triesLeft === 0){
+            guessResults.innerHTML = `Out of tries!<br>
+            The number was: ${randNum}<br>
+            GAME OVER!`, gameOver()
+        }
         else{
             guessResults.innerHTML = `You guessed: ${userChoice}<br>
-             The number was ${randNum}<br>
-              You Lose!`, guessResults.style.color='red', guessCount++
+             Wrong number! Tries left: ${triesLeft}`,
+              guessResults.style.color='red', guessCount++, e.target.style.color ='red' , e.target.style.outlineColor ='red', 
+              triesLeft--
         }
         
-        guessCountArea.innerHTML = `Current guesses: ${guessCount}` 
         correctArea.innerHTML = `Correct guesses: ${numRight}`
         const guessPercent =  (numRight / totGuesses) *100
         luckArea.innerHTML = `Percentage of correct guesses: ${Math.round(100*guessPercent) / 100}%`
 
+       
 })
 })
+
---
+++ guessing game/objects.js
@@ -0,0 +1,53 @@
+let guessResults = document.querySelector("#results");
+const guessCountArea = document.querySelector("#guesses");
+const fTryArea = document.querySelector("#firstTry");
+const correctArea = document.querySelector("#correct");
+const totGuessesArea = document.querySelector("#total");
+const luckArea = document.querySelector("#luck");
+const tryArea = document.querySelector("#try");
+let guessCount = 0;
+let fTry = 0;
+let numRight = 0;
+let totGuesses = 0;
+let triesLeft = 4;
+
+let randNum = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector("#rButton").addEventListener("click", () => {
+  location.reload();
+});
+
+const allBoxes = document.querySelectorAll(".box");
+
+const boxesDisabled = () => {
+  document.querySelector('*').style.cursor= "progress"
+  allBoxes.forEach((box) => {
+    box.disabled = true;
+    box.style.color = "grey";
+    box.style.outlineColor = "grey";
+  });
+};
+
+const gameOver = () => {
+  allBoxes.forEach((box) => {
+    box.disabled = true;
+    box.style.color = "grey";
+    box.style.outlineColor = "grey";
+    box.style.cursor = "not-allowed";
+  });
+};
+
+const gameWinTimeout = () => {
+  setTimeout(() => {
+    guessResults.innerHTML = "";
+    triesLeft = 4;
+    boxesDisabled(false);
+    document.querySelector('*').style.cursor= "default"
+    allBoxes.forEach((box) => {
+      box.style.color = "white";
+      box.style.outlineColor = "white";
+      box.disabled = false;
+      box.style.cursor = "not-allowed";
+    });
+  }, 4100);
+};
